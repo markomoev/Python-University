c0b6c4bf5a3d8dad74ef2b7e480fe1cf7f79be95
Marko Moev <marko.moev.business@gmail.com>
2025-12-02 15:46:39 +0200

    final exercises at lab

--- lab/task13.py
+++ lab/task13.py
@@ -1,5 +1,5 @@
-with open("data.txt", 'r', encoding="utf-8") as f:
-    for line in f:
-        if "Python" in line:
-            python_line = line
-            print(python_line)
+num = int(input("napishi chislo e maina: "))
+
+with open("hello.txt", 'r') as f:
+    a = f.read(num)
+    print(a)
